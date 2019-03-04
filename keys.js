1d51f6f05fcc0eb51e484216f9cd47b2bce11635
Ellysha Chavez <ellyshachavez@gmail.com>
2019-03-03 22:32:43 -0800

    prompts

--- keys.js
+++ keys.js
@@ -1,5 +1,14 @@
 
 console.log('this is loaded');
+var prompt = [
+  "Choose any of these commands: ",
+"concert-this",
+"spotify-this-song",
+"movie-this",
+"do-what-it-says"
+]
+
+console.log(prompt)
 
 exports.spotify = {
     id: process.env.SPOTIFY_ID,
--- liri.js
+++ liri.js
@@ -39,7 +39,7 @@ function runLiri(commands, searchTerm){
         case "do-what-it-says":
             doWhatItSays();
             break;
-        default: console.log("Try typing 'node liri.js do-what-it-says'");
+        
     };
 }
 
@@ -100,7 +100,55 @@ function spotifyThisSong(song) {
 
 // If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
 
+function moviethis(movie){
 
+    if(!movie){
+        movie = "Mr. Nobody"
+      
+        console.log("======================================");
+
+        console.log("If you haven't watched 'Mr. Nobody', then you should: http://www.imdb.com/title/tt0485947/");
+        console.log ("It's on Netflix!");
+
+        console.log("======================================");
+    }
+
+    params = movie 
+
+    request ("http://www.omdbapi.com/?t=" + params + "&y=&plot=short&r=json&tomatoes=true&apikey=trilogy", function(error, response, body){
+        if (!error && response.statusCode == 200){
+
+            var movies = JSON.parse(body);
+
+            console.log("======================================");
+
+            console.log("Title: " + movies.Title);
+            console.log("Release Year: " + movies.Year);
+            console.log("IMDB Rating: " + movies.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + movies.tomatoRating);
+            console.log("Country: " + movies.Country);
+            console.log("Language: " + movies.Language);
+            console.log("Plot: " + movies.Plot);
+            console.log("Actors: " + movies.Actors);
+
+            console.log("======================================");
+        } 
+        else {
+            console.log("Error :" + error);
+            return;
+        }
+    });
+};
+
+//----------------Concert----------------
+
+// node liri.js concert-this <artist/band name here>
+
+// This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
+
+// Name of the venue
+// Venue location
+// Date of the Event (use moment to format this as "MM/DD/YYYY")
 
 //----------------Do What It Says----------------
 
